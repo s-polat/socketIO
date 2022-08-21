5177143d015a4ceb18bd71ba9ccb90104d51631e
s-polat <spolatde@gmail.com>
2022-08-21 15:12:13 +0200

    card and navbar components are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  console.log(username);
+  console.log(user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="login">
+        <input type="text" placeholder="username" onChange={(e)=>setUsername(e.target.value)}/>
+        <button onClick={()=>setUser(username)} >Login</button>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/card/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './card.css'
+
+function Card() {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
