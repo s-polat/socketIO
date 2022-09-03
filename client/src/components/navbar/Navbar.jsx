07c48763d164fcea48e031bca216bea7f0ba21ec
s-polat <spolatde@gmail.com>
2022-09-03 20:49:26 +0200

    last changed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,40 +1,60 @@
-import React, { useState } from 'react'
-import './navbar.css'
-import Notification from '../../img/bell-solid.svg'
-import Message from '../../img/message.svg'
-import Settings from '../../img/settings.svg'
-import { useEffect } from 'react'
+import React, { useState } from "react";
+import "./navbar.css";
+import Notification from "../../img/bell-solid.svg";
+import Message from "../../img/message.svg";
+import Settings from "../../img/settings.svg";
+import { useEffect } from "react";
 
-function Navbar({socket}) {
-    const [notifications, setNotifications] = useState([])
+function Navbar({ socket }) {
+  const [notifications, setNotifications] = useState([]);
+  const [open, setOpen] = useState(false);
 
-    useEffect(()=>{
-        socket.on("getNotification", (data)=>{
-            setNotifications((prev)=>[...prev, data])
-        })
+  useEffect(() => {
+    socket.on("getNotification", (data) => {
+      setNotifications((prev) => [...prev, data]);
+    });
+  }, [socket]);
 
-    },[socket])
+  console.log("notifications", notifications);
 
-    console.log("notifications",notifications);
+  const displayNotifications = ({ senderName, type }) => {
+    let action;
+    if (type === 1) {
+      action = "liked";
+    } else if (type === 2) {
+      action = "commented";
+    } else {
+      action = "shared";
+    }
+    return (
+      <span className="notification">{`${senderName} ${action} your post`}</span>
+    );
+  };
+
   return (
-    <div className='navbar'>
-        <span className='logo'>Chat </span>
-        <div className="icons">
-            <div className="icon">
-                <img src={Notification} className='iconImg' alt="" />
-                <div className="counter">2</div>
-            </div>
-            <div className="icon">
-                <img src={Message} className='iconImg' alt="" />
-                <div className="counter">2</div>
-            </div>
-            <div className="icon">
-                <img src={Settings} className='iconImg' alt="" />
-                <div className="counter">2</div>
-            </div>
+    <div className="navbar">
+      <span className="logo">Chat </span>
+      <div className="icons">
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Notification} className="iconImg" alt="" />
+          <div className="counter">2</div>
         </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Message} className="iconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Settings} className="iconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+      </div>
+      {open && (
+        <div className="notifications">
+          {notifications.map((n) => displayNotifications(n))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
